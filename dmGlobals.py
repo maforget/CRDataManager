8a57d50f789cc2db68ecefc870359018cb093310
maforget <11904426+maforget@users.noreply.github.com>
2022-04-05 18:24:29 -0400

    Fixed CaseSensitive location to user.ini

--- dmGlobals.py
+++ dmGlobals.py
@@ -56,7 +56,7 @@ DONATE = 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=U
 WIKI = 'http://code.google.com/p/cr-data-manager/'
 MANUAL = 'http://code.google.com/p/cr-data-manager/downloads/list'
 
-COMPARE_CASE_SENSITIVE = ReadKeyAsBool(INIFILE, 'CaseSensitive')
+COMPARE_CASE_SENSITIVE = ReadKeyAsBool(USERINI, 'CaseSensitive')
 
 
 #############End set constant variables##################
