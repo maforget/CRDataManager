da5ff3b07b3b6bdb9b057960e23486d05b45038e
maforget <11904426+maforget@users.noreply.github.com>
2023-06-30 01:49:13 -0400

    Change path of output crplugin

--- CreatePlugin.py
+++ CreatePlugin.py
@@ -72,8 +72,9 @@ def get_plugin_name():
     
     name = thedict["Name"]
     version = thedict["Version"]
-    desktop = os.path.expanduser("~/Desktop")
-    out_name = os.path.join(desktop, f"{name}_v{version}.crplugin")
+    # desktop = os.path.expanduser("~/Desktop")
+    dir = os.path.dirname(__file__)
+    out_name = os.path.join(dir, f"{name}_v{version}.crplugin")
     print(out_name)
     return out_name
 
