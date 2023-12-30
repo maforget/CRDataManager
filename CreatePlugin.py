428b0723b2810ec723564b463823736d1ac8a1e5
maforget <11904426+maforget@users.noreply.github.com>
2023-12-30 00:30:57 -0500

    Updated CreatePlugin

--- CreatePlugin.py
+++ CreatePlugin.py
@@ -72,8 +72,8 @@ def get_plugin_name():
     
     name = thedict["Name"]
     version = thedict["Version"]
-    # desktop = os.path.expanduser("~/Desktop")
-    dir = os.path.dirname(__file__)
+    dir = os.path.expanduser("~/Desktop")
+    # dir = os.path.dirname(__file__)
     out_name = os.path.join(dir, f"{name}_v{version}.crplugin")
     print(out_name)
     return out_name
